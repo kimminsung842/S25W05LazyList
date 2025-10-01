2077438be2a093f01f79e931ff8688da991471ed
domi12s <117494918+domi12s@users.noreply.github.com>
2025-10-01 15:33:29 +0900

    🏗️MVVM

--- app/src/main/java/kr/ac/kumoh/s20210181/s25w05lazylist/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20210181/s25w05lazylist/MainActivity.kt
@@ -12,14 +12,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import kr.ac.kumoh.s20210181.s25w05lazylist.model.Song
 import kr.ac.kumoh.s20210181.s25w05lazylist.ui.theme.S25W05LazyListTheme
+import kr.ac.kumoh.s20210181.s25w05lazylist.viewmodel.SongViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,24 +40,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainScreen() {
+fun MainScreen(
+    viewModel: SongViewModel = viewModel()
+) {
+    val listSong by viewModel.songs.collectAsState()
+
+    repeat(30) { index ->
+        viewModel.add(Song(index * 3, "Golden", "HUNTR/X"))
+        viewModel.add(Song(index * 3 + 1, "Drowning", "WOODZ"))
+        viewModel.add(Song(index * 3 + 2, "Soda Pop", "Saja Boys"))
+    }
+
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
         MyList(
-            modifier = Modifier.padding(innerPadding)
+            modifier = Modifier.padding(innerPadding),
+            songs = listSong,
         )
     }
 }
 
 @Composable
-fun SongItem(index: Int) {
+fun SongItem(song: Song) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .background(Color(0xffffffcc))
             .padding(16.dp)
     ) {
-        TextTitle("노래 $index")
-        TextSinger("이 노래를 부른 가수는 $index 입니다")
+        TextTitle(song.title)
+        TextSinger(song.singer)
     }
 }
 
@@ -66,13 +83,16 @@ fun TextSinger(singer: String) {
 }
 
 @Composable
-fun MyList(modifier: Modifier = Modifier) {
+fun MyList(
+    modifier: Modifier = Modifier,
+    songs: List<Song>,
+) {
     LazyColumn(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(8.dp),
         contentPadding = PaddingValues(horizontal = 8.dp)
     ) {
-        items(30) {
+        items(songs) {
             SongItem(it)
         }
     }
